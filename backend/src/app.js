b89b69b72bed332dbcc4c651db9b1e11266d0d87
Anurag Basuri <anuragbasuri2502@gmail.com>
2024-10-02 03:56:55 +0530

    use cors and cookie-parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,19 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
 
 // Add your middleware, routes, etc. here
-// app.use(...)
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({extended: true, limit: "20kb" }));
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export { app };
