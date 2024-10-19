77384efb305c8ebccc9ba60cff5a7c292e65dedf
Anurag Basuri <anuragbasuri2502@gmail.com>
2024-10-19 11:31:44 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import { asynchandler } from "../utils/asynchandler.js";
 
 const registerUser = asynchandler(async (req, res) => {
-    res.status(200).json({
-        message: "This is working",
-    });
+    // get user details from the frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images and avatar
+    // upload them to the cloudinary, avatar
+    // create user object - create entry in DB
+    // remove password and refresh token field from response
+    // check user creation
+    // return res
 });
 
 export { registerUser };
