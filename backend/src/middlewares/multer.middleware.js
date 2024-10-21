518111dece8cac90763699d3d17cf3358c66da25
Anurag Basuri <anuragbasuri2502@gmail.com>
2024-10-21 19:36:59 +0530

    multer update

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,11 +1,20 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp");
+        const dir = "./public/temp";
+
+        // Check if the directory exists, if not, create it
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true }); // Create the directory recursively if it doesn't exist
+        }
+
+        cb(null, dir); // Pass the directory to multer
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname);
+        cb(null, file.originalname); // Use the original name of the file
     },
 });
 
