0bbb3fec14833bf5a5e12ad1a278ecdb8f98e5ca
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2024-12-04 16:19:12 +0530

    comment controller is completed

---
+++ backend/src/routes/comment.routes.js
@@ -0,0 +1,48 @@
+import { Router } from "express";
+import { body } from "express-validator";
+import { verifyAccessToken } from "../middlewares/auth.middleware";
+import { validateResult } from "../middlewares/validate.middleware";
+import {
+    addComment,
+    deleteComment,
+    getEntityComments,
+    updateComment,
+} from "../controllers/comment.controller";
+
+const router = Router();
+
+// verify if user is login
+router.use(verifyAccessToken);
+
+// get a comment
+router.get("/:entityType/:entityId", getEntityComments);
+
+// add a comment
+router
+    .route("/:entityType/:entityId")
+    .post(
+        body("content")
+            .isString()
+            .trim()
+            .notEmpty()
+            .withMessage("Comment is required"),
+        validateResult,
+        addComment
+    );
+
+// update a comment
+router.put(
+    "/:commentId",
+    body("content")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("Comment is required"),
+    validateResult,
+    updateComment
+);
+
+// delete a comment
+router.delete("/:commentId", deleteComment);
+
+export default router;
