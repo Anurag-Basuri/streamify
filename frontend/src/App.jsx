4785e6b2667c55feac841ea35a048027c9867746
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2025-01-15 17:51:51 +0530

    Making Header and completed with all setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
