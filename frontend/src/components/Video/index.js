8969bf6923031b40e325003c9443e8f02cff39fb
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-01-12 11:14:59 +0530

    feat(Video): create index file for central export of video-related components

---
+++ frontend/src/components/Video/index.js
@@ -0,0 +1,20 @@
+/**
+ * Video Components Index
+ * Central export for all video-related components
+ */
+
+// Main video display
+export { default as VideoCard } from "./VideoCard";
+export { default as VideoPlayer } from "./VideoPlayer";
+export { default as VideoInfo } from "./VideoInfo";
+
+// Skeleton and loading states
+export { default as VideoCardSkeleton } from "./VideoCardSkeleton";
+export { default as LoadingState } from "./LoadingState";
+
+// Error and empty states
+export { default as ErrorState } from "./ErrorState";
+export { default as VideoEmptyState } from "./EmptyState";
+
+// Actions
+export { default as WatchLaterButton } from "./WatchLaterButton";
